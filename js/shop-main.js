0cb1f314699823d167958e255cc7f11391aea77d
PaweliOS <mirrorpawel.pabisiak@gmail.com>
2023-09-17 21:30:13 +0200

    Changed source of product data from static html to simple array with the products, made necessary changes on front.

--- js/shop-main.js
+++ js/shop-main.js
@@ -21,7 +21,24 @@ const mainShopBoard = document.querySelector('.main__shop-board');
 
 const renderProducts = (items) => {
     for (let i = 0; i < items.length; i++) {
-
+        console.log('iterator: ', i);
+        const newProduct = document.createElement('div');
+        newProduct.className = `card m-4 main__shop-board-cards`;
+        newProduct.innerHTML = `
+                        <img src="${items[i].images}" class="card-img-top" alt="zdjęcie produktu">
+                        <div class="card-body">
+                            <h5 class="card-title">${items[i].name}</h5>
+                            <p class="card-text">${items[i].description}</p>
+                            <div class="d-flex flex-column align-items-center  fs-4">
+                                <div class="d-flex">
+                                    <p class="me-2 mb-0" for="">${items[i].price.toFixed(2)}</p>
+                                    <p class="mb-0" for="">zł</p>
+                                </div>
+                                <a href="./shop.html" class="btn btn-primary mx-auto">do koszyka</a>        
+                            </div>                            
+                        </div>`
+        
+        mainShopBoard.appendChild(newProduct);
     }
 }
 
